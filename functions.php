5cc8fecb6201215e4161969aa8033bae1a0095df
Ljamov <krki666@gmail.com>
2022-03-28 22:05:41 +0200

    Added bootstrap css

--- functions.php
+++ functions.php
@@ -138,6 +138,8 @@ add_action( 'widgets_init', 'demo_theme_widgets_init' );
  * Enqueue scripts and styles.
  */
 function demo_theme_scripts() {
+	wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css', array(), _S_VERSION );
+	
 	wp_enqueue_style( 'demo-theme-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'demo-theme-style', 'rtl', 'replace' );
 
