0166cf84c72371b6e121f2c483073f74c899fa5b
Ljamov <krki666@gmail.com>
2022-03-30 22:41:47 +0200

    Added markup in footer

--- footer.php
+++ footer.php
@@ -11,20 +11,47 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'demo-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'demo-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'demo-theme' ), 'demo-theme', '<a href="https://ljamov.com">Kristijan Ljamov</a>' );
-				?>
-		</div><!-- .site-info -->
+	<footer id="colophon" class="footer">
+		<div class="container">
+			<div class="footer__top">
+				<div>
+					<img src="">
+				</div>
+
+				<div>
+					<nav>
+						<ul class="nav nav--secondary">
+							<li>
+								<a href="#">Home</a>
+							</li>
+							<li>
+								<a href="#">Features</a>
+							</li>
+							<li>
+								<a href="#">Reviews</a>
+							</li>
+							<li>
+								<a href="#">Download</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+
+				<div>
+					<nav>
+						<ul class="social-icons">
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+				
+				<div class="footer__bottom">
+					<p>&copy; 2016 All rights reserved. Weather App Theme by Gravpix</p>
+				</div>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
